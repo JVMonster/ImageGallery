2b80aa95b80da9585fc65cdba360e6c16cc59c69
JVMonster <yabgaryan@bk.ru>
2016-08-22 12:18:32 +0400

    first commit

---
+++ app/src/main/java/aca/com/imagegallery/Activities/GalleryActivity.java
@@ -0,0 +1,9 @@
+package aca.com.imagegallery.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Yur on 21/08/16.
+ */
+public class GalleryActivity extends AppCompatActivity {
+}
