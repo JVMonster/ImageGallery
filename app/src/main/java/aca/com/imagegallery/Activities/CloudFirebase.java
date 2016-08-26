50e1b9e1a09c1a4760c86265558d4b50368611b2
JVMonster <yabgaryan@bk.ru>
2016-08-27 01:26:15 +0400

    SECond commit

---
+++ app/src/main/java/aca/com/imagegallery/Activities/CloudFirebase.java
@@ -0,0 +1,9 @@
+package aca.com.imagegallery.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Yur on 27/08/16.
+ */
+public class CloudFirebase extends AppCompatActivity {
+}
